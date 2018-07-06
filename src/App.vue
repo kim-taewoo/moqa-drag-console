233a27a636ec0640b33f2b6b252ca85b633c4d81
Taewoo Kim <acoustic0419@gmail.com>
2018-07-06 14:32:22 +0900

    edit-console without v-flex

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      temporary
+      v-model="drawer"
+      enable-resize-watcher
+      app
+    >
+      <v-list>
+        <v-list-tile
+          value="true"
+          v-for="(item, i) in items"
+          :key="i"
+        >
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar
+      app
+      flat
+      class="primary"
+      dark
+      dense
+    >
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      drawer: false,
+      items: [{
+        icon: 'bubble_chart',
+        title: 'Inspire'
+      }],
+      title: 'MOQA!'
+    }
+  },
+  name: 'App'
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-container fluid full-height class="pa-2">
+    <v-layout>
+      <v-card id="edit-console" class="ma-2 pa-2" height="100%">
+        <v-container class="pa-0">
+          <v-layout row wrap>
+            <draggable :list="types" :options="{draggable: '.drag-item'}">
+              <v-card class="ma-1 pa-1 secondary drag-item" dark text v-for="type in types" :key="type.identifier">
+                <v-card-title class="py-0 px-1">
+                  <v-icon>add</v-icon>
+                  <div class="title">{{ type.title }}</div>
+                </v-card-title>
+              </v-card>
+            </draggable>
+          </v-layout>
+        </v-container>
+      </v-card>
+      <v-card id="main-workplace" class="ma-2 pa-2" height="100%">
+
+      </v-card>
+      <v-card id="survey-preview" class="ma-2 pa-2" height="100%">
+
+      </v-card>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  import draggable from 'vuedraggable'
+
+  export default {
+    components: {
+      draggable
+    },
+    data () {
+      return {
+        types: [
+          { identifier: 0, title: '객관식' },
+          { identifier: 1, title: '주관식' },
+          { identifier: 2, title: '별점형' },
+          { identifier: 3, title: '동영상' },
+          { identifier: 4, title: '퀴즈형' },
+          { identifier: 5, title: '순위형' },
+          { identifier: 6, title: '링크' }
+        ]
+      }
+    }
+  }  
+</script>
+
+<style>
+  #edit-console {
+    min-height: 505px;
+    width: 20%;
+  }
+   #main-workplace {
+     min-height: 505px;
+     width: 55%;
+   }
+   #survey-preview {
+     min-height: 505px;
+     width: 25%
+   }
+</style>
